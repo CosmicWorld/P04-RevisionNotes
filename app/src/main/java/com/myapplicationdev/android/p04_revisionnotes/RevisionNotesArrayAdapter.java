b92c2ab03d046268f010b107e4b2faec4d3281a5
CosmicWorld <15017381@myrp.edu.sg>
2017-05-12 15:36:55 +0800

    Solution Completed!!!

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/RevisionNotesArrayAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/RevisionNotesArrayAdapter.java
@@ -8,6 +8,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 public class RevisionNotesArrayAdapter extends ArrayAdapter<Note> {
@@ -15,6 +17,7 @@ public class RevisionNotesArrayAdapter extends ArrayAdapter<Note> {
 	ArrayList<Note> notes;
 	int resource;
 	ImageView iv1, iv2, iv3, iv4, iv5;
+	TextView tvContent;
 
 	public RevisionNotesArrayAdapter(Context context, int resource, ArrayList<Note> notes) {
 		super(context, resource, notes);
@@ -30,22 +33,37 @@ public class RevisionNotesArrayAdapter extends ArrayAdapter<Note> {
 
 		View rowView = inflater.inflate(resource, parent, false);
 
-		//Match the UI components with Java variables
+		iv1 = (ImageView)rowView.findViewById(R.id.imageView1star);
+		iv2 = (ImageView)rowView.findViewById(R.id.imageView2star);
+		iv3 = (ImageView)rowView.findViewById(R.id.imageView3star);
+		iv4 = (ImageView)rowView.findViewById(R.id.imageView4star);
+		iv5 = (ImageView)rowView.findViewById(R.id.imageView5star);
+		tvContent = (TextView)rowView.findViewById(R.id.textViewNote);
 
 		Note note = notes.get(position);
+		int star = note.getStars();
+		tvContent.setText(note.getContent());
 
-		//Check if the property for starts >= 5, if so, "light" up the stars
-		if (/*stars >= 5*/) {
+		if (star == 5) {
 			iv5.setImageResource(android.R.drawable.btn_star_big_on);
+		}
+
+		if (star >= 4) {
 			iv4.setImageResource(android.R.drawable.btn_star_big_on);
+		}
+
+		if (star >= 3) {
 			iv3.setImageResource(android.R.drawable.btn_star_big_on);
+		}
+
+		if (star >= 2) {
 			iv2.setImageResource(android.R.drawable.btn_star_big_on);
+		}
+
+		if (star >= 1) {
 			iv1.setImageResource(android.R.drawable.btn_star_big_on);
 		}
 
 		return rowView;
 	}
-
-
-
 }
